c4a47d63ac3d6557ab699e657db534161547f776
rupesh palve <rupesh.palve48@outlook.com>
2022-09-30 15:07:43 -0400

    Add project files.

---
+++ StrBuildMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrBuildMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder("hello to all");
+            float salary = 25000.00f;
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Append("My name is dipali");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.AppendFormat("my salary is {0:C}", salary);
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Insert(10, "Hii");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Replace("dipali", "Dipu");
+            Console.WriteLine(stringBuilder);
+
+            stringBuilder.Remove(10, 5);
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
